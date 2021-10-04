9d948041eea43f9fc5d62fdd5b621192a7db9ad7
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-04 21:49:12 +0200

    Add DisemvowelTrolls exercise which still needs a few adjustements.

---
+++ 7kyu/DisemvowelTrolls/DisemvowelTrolls/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DisemvowelTrolls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comment1 = "This website is for losers LOL!";
+            string comment2 = "No offense but,\nYour writing is among the worst I've ever read";
+            string comment3 = "What are you, a communist?";
+            List<char> charsToRemove = new List<char>()
+            {
+                'a', 'e', 'i', 'o', 'u'
+            };
+            
+            Console.WriteLine(Disemvowel(comment1, charsToRemove));
+        }
+
+        public static string Disemvowel(string trollComment, List<char> charsToRemove)
+        {
+            foreach (char c in charsToRemove)
+            {
+                trollComment = trollComment.Replace(c.ToString(), String.Empty);
+            }
+            return trollComment;
+        }
+    }
+}
