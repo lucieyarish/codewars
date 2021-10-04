fc03344ca15a32260d370f71daa3339e652e965b
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-04 16:02:21 +0200

    Add EvenOrOdd solution.

---
+++ 8 kyu/EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenOrOdd(2));
+            Console.WriteLine(EvenOrOdd(1));
+            Console.WriteLine(EvenOrOdd(0));
+            Console.WriteLine(EvenOrOdd(7));
+            Console.WriteLine(EvenOrOdd(-1));
+        }
+
+        public static string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+    }
+}
