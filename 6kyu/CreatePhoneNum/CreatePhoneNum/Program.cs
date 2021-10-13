afe13e4853e3773e3af364c3a5625f6b56c5f9d3
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-13 17:46:50 +0200

    Add CreatePhoneNumber solution.

---
+++ 6kyu/CreatePhoneNum/CreatePhoneNum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CreatePhoneNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+            //return (123) 456-7890
+            Console.WriteLine(CreatePhoneNumber(numbers));
+        }
+
+        public static string CreatePhoneNumber(int[] numbers) 
+        {
+            string joinedString = String.Join(String.Empty, numbers);
+            string phoneNumber = String.Format("{0:(###) ###-####}", Convert.ToInt64(joinedString));
+
+            return phoneNumber;
+        }
+    }
+}
