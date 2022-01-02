6d8656134c34aa7b1ec2282c363ba4b635789c55
Lucie Yarish <lucieyarish@hotmail.com>
2022-01-02 11:09:04 +0100

    Add WhoLikesIt solution

---
+++ 6kyu/WhoLikesIt/WhoLikesIt/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace WhoLikesIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] emptyArr = new string[0]; //"no one likes this"
+            string[] onePersonLikes = new[] {"Peter"}; // "Peter likes this"
+            string[] twoPeopleLike = new[] {"Jacob", "Alex"}; //"Jacob and Alex like this"
+            string[] threePeopleLike = new[] {"Max", "John", "Mark"}; // "Max, John and Mark like this"
+            
+            //For 4 or more names, the number in "and 2 others" simply increases
+            string[] fourPeopleLike = new[] {"Alex", "Jacob", "Mark", "Max"}; //"Alex, Jacob and 2 others like this"
+
+            Console.WriteLine(Likes(fourPeopleLike));
+        }
+
+        public static string Likes(string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names.Length == 1)
+                {
+                    return $"{names[i]} likes this";
+                }
+                
+                if(names.Length == 2)
+                {
+                    return $"{names[i]} and {names[i + 1]} like this";
+                }
+
+                if (names.Length == 3)
+                {
+                    return $"{names[i]}, {names[i + 1]} and {names[i + 2]} like this";
+                }
+
+                if (names.Length >= 4)
+                {
+                    return $"{names[i]}, {names[i + 1]} and {names.Length - 2} others like this";
+                }
+            }
+            
+            return "no one likes this";
+        }
+    }
+}
