f3f8043007f4952bcfc132f6589f412e51558202
Lucie Yarish <lucieyarish@hotmail.com>
2022-01-05 22:35:20 +0100

    Add algo solution

---
+++ 5kyu/FirstNonRepeatingChar/FirstNonRepeatingChar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstNonRepeatingChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "stress"; //returns "t"
+            string input2 = "sTreSS"; //returns "T"
+
+            FirstNonRepeatingLetter(input1);
+        }
+
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            char[] myCharArr = s.ToCharArray();
+
+            Dictionary<int, char> letterDict = new Dictionary<int, char>();
+            for (int i = 0; i < myCharArr.Length; i++)
+            {
+                letterDict.Add(i + 1, myCharArr[i]);
+            }
+            return s.Substring(0, 1);
+        }
+    }
+}
---
+++ 6kyu/MakeTheDeadFishSwim/MakeTheDeadFishSwim/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Channels;
+using Microsoft.VisualBasic;
+
+namespace MakeTheDeadFishSwim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // i = ++, d = --, s = n*n, o = output the value into the return arr
+            string myString = "iiisdoso"; //returns new int[] { 8, 64};
+
+            int[] resultArr = Parse(myString);
+            foreach (var VARIABLE in resultArr)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+
+        static int[] Parse(string input)
+        {
+            char[] letters = input.ToCharArray();
+            int initVal = 0;
+            List<int> outputNums = new List<int>();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] == 'i')
+                {
+                    initVal++;
+                } 
+                else if (letters[i] == 'd')
+                {
+                    initVal--;
+                }
+                else if (letters[i] == 's')
+                {
+                    initVal *= initVal;
+                }
+                else if (letters[i] == 'o')
+                {
+                    outputNums.Add(initVal);
+                }
+            }
+
+            return outputNums.ToArray();
+        }
+    }
+}
