e45aa5fd927dde165aedb89ee950bf38027476e5
Lucie Yarish <lucieyarish@hotmail.com>
2022-01-25 17:29:04 +0100

    Add solution

---
+++ 6kyu/WhichAreIn/WhichAreIn/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace WhichAreIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = new[] { "arp", "live", "strong" };
+            string[] arr2 = new[] { "lively", "alive", "harp", "sharp", "armstrong" };
+
+            foreach (var VARIABLE in GetLexSort(arr1, arr2))
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+
+        public static string[] GetLexSort(string[] input1, string[] input2)
+        {
+            List<string> substrings = new List<string>();
+            for (int i = 0; i < input2.Length; i++)
+            {
+                for (int j = 0; j < input1.Length; j++)
+                {
+                    if (input2[i].Contains(input1[j]) && !substrings.Contains(input1[j]))
+                    {
+                        substrings.Add(input1[j]);
+                    }
+                }
+            }
+            
+            substrings.Sort();
+            
+            return substrings.ToArray();
+        }
+    }
+}
---
+++ OtherAlgoChallenges/IsSymmetricOrNot/IsSymmetricOrNot/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace IsSymmetricOrNot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]
+            {
+                {1, 0, 1, 1},
+                {0, 4, 2, 2},
+                {1, 2, 3, 5},
+                {1, 2, 5, 6}
+            };
+
+            int[,] matrix2 = new int[,]
+            {
+                {1, 3, 1, 1},
+                {2, 4, 2, 2},
+                {1, 2, 3, 5},
+                {1, 2, 5, 6}
+            };
+
+            //Console.WriteLine(IsSymmetric(matrix)); //returns true
+            //IsSymmetric(matrix2); returns false
+            
+            GetDiagonal();
+        }
+
+        public static void GetDiagonal()
+        {
+            for (int row = 0; row < 6; row++)
+            {
+                for (int col = 0; col < 6; col++)
+                {
+                    if (row == col)
+                    {
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+                }
+                Console.WriteLine();  
+            }
+        }
+        
+        public static bool IsSymmetric(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] != matrix[j,i])
+                    {
+                        return false;
+                    }
+                }   
+            }
+
+            return true;
+        }
+    }
+}
